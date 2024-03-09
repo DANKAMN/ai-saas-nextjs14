f5d72355edacf72662b80341817823685a5b32ef
DANKAMN <danielnicholas1477@gmail.com>
2024-03-09 09:43:46 +0100

    Add Image Form (React Hook Form + Zod Validation)

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,14 +1,35 @@
 import Header from '@/components/Shared/Header'
 import React from 'react'
 import { transformationTypes } from '@/constants'
+import TransformationForm from '@/components/Shared/TransformationForm'
+import { auth } from '@clerk/nextjs'
+import { getUserById } from '@/lib/actions/user.actions'
+import { redirect } from 'next/navigation'
 
-const AddTransformationTypePage = ({ params: { type }} : SearchParamProps) => {
+const AddTransformationTypePage = async ({ params: { type }} : SearchParamProps) => {
+  const { userId } = auth()
+
   const transformation = transformationTypes[type]
+
+  if(!userId) redirect('/sign-in')
+
+  const user = await getUserById(userId)
   return (
-    <Header 
-      title={transformation.title} 
-      subtitle={transformation.subTitle}
-    />
+    <>
+      <Header 
+        title={transformation.title} 
+        subtitle={transformation.subTitle}
+      />
+
+      <section className='mt-10'>
+        <TransformationForm
+          action='Add'
+          userId={user._id}
+          type={transformation.type as TransformationTypeKey}
+          creditBalance={user.creditBalance}
+        />
+      </section>
+    </>
   )
 }
 
