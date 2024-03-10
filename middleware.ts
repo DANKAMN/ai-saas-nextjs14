2e1520d0c411a57b77323646d7b9f7fc5654c109
Daniel Nicholas <84849790+DANKAMN@users.noreply.github.com>
2024-03-10 19:06:36 +0100

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
  
 export default authMiddleware({
-  publicRoutes: ['/', '/api/webhooks/clerk', '/api/webhooks/stripe','/transformations/65eddbc32138891426b75684']
+  publicRoutes: ['/', '/api/webhooks/clerk', '/api/webhooks/stripe']
 });
  
 export const config = {
